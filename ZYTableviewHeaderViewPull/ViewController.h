71d061e65e0a8d2ba486810eeabc6ddec54f9674
yi <zhengyi@tinckay.com>
2016-02-14 15:47:26 +0800

    tableView下拉headview放大

--- ZYTableviewHeaderViewPull/ViewController.h
+++ ZYTableviewHeaderViewPull/ViewController.h
@@ -1,6 +1,6 @@
 //
 //  ViewController.h
-//  ZYTableviewHeaderViewPull
+//  ZYTabelViewHeader
 //
 //  Created by yi on 16/2/14.
 //  Copyright © 2016年 yi. All rights reserved.
--- ZYTableviewHeaderViewPull/ViewController.m
+++ ZYTableviewHeaderViewPull/ViewController.m
@@ -1,14 +1,16 @@
 //
 //  ViewController.m
-//  ZYTableviewHeaderViewPull
+//  ZYTabelViewHeader
 //
 //  Created by yi on 16/2/14.
 //  Copyright © 2016年 yi. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "ParallaxHeaderView.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (strong,nonatomic) UITableView *tableView;
 
 @end
 
@@ -17,7 +19,67 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self createTableView];
+
 }
+
+
+-(void)createTableView
+{
+    //1.定义全局tableView
+    
+    //2.初始化_tableView
+    self.tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    //3.设置代理，头文件也要包含 UITableViewDelegate,UITableViewDataSource
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    //4.设置contentInset属性（上左下右 的值）
+    [self.view addSubview:self.tableView];
+    
+    ParallaxHeaderView *headerView = [ParallaxHeaderView parallaxHeaderViewWithCGSize:CGSizeMake(self.tableView.frame.size.width, 200)];
+    headerView.headerTitleLabel.text = @"下拉头部放大";
+    headerView.headerImage = [UIImage imageNamed:@"FriendsBackground"];
+    
+    [self.tableView setTableHeaderView:headerView];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    if (scrollView == self.tableView)
+    {
+        // pass the current offset of the UITableView so that the ParallaxHeaderView layouts the subViews.
+        [(ParallaxHeaderView *)self.tableView.tableHeaderView layoutHeaderViewForScrollViewOffset:scrollView.contentOffset];
+    }
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellId = @"cellId";
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:cellId];
+    if(cell==nil){
+        cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.separatorInset=UIEdgeInsetsZero;
+        cell.clipsToBounds = YES;
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"第 %ld 行",indexPath.row];
+    
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
